14d297aabf63cff7305ffe775182defe7c6e33d3
Cristian Lopez <lopezacostacristianeduver@gmail.com>
2026-02-03 12:11:36 -0500

    fix: port dinámico y health check

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`🚀 Server running on port ${port}`);
 }
+bootstrap();
